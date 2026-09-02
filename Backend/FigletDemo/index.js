e9ad5808ab1d6393e929952ed7fffea3d5ec3d6a
Hemanth.L <hemanthl1307@gmail.com>
2026-09-02 18:50:24 +0530

    Added figlet npm learning files

---
+++ Backend/FigletDemo/index.js
@@ -0,0 +1,9 @@
+let figlet=require("figlet");
+figlet("Narasiyappa", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
